fe5ac65391127157fd25c87c07b65ca5e7b36330
Reilly Shofner <reilly.shofner@gmail.com>
2024-05-07 14:58:25 -0700

    complete sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+
+function submitData(name, email) {
+   return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(function (response) {
+return response.json();
+    })
+    .then(function (object) {
+     const newID = object.id;
+     const createdElement = document.createElement("p");
+     createdElement.textContent = `New ID: ${newID}`
+     document.body.appendChild(createdElement)
+    })
+    .catch(function (error) {
+        const createdElement2 = document.createElement("p");
+        const  errorMessage = `sorry ${name}! An error has occured! Unauthorized Access`;
+        createdElement2.textContent = errorMessage;
+        document.body.appendChild(createdElement2);
+    });
+}
+
+
+
+
+
+
+
